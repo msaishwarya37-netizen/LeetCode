2e01cbffb27ff9d0b48b5828c4f4b928c5ad87ae
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-19 23:47:45 +0530

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -0,0 +1,19 @@
+// Last updated: 7/19/2026, 11:46:51 PM
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int prefixsum=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            prefixsum+=nums[i];
+        
+        if(map.containsKey(prefixsum-k)){
+            count+=map.get(prefixsum-k);
+        }
+        map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
+    }
+    return count;
+}
+}
