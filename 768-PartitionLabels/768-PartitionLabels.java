8dc5de5becb2d14f5d4780f483a0dafb7ca8bdaf
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-19 23:47:38 +0530

    Create 768-PartitionLabels

---
+++ 768-PartitionLabels/768-PartitionLabels.java
@@ -0,0 +1,21 @@
+// Last updated: 7/19/2026, 11:46:45 PM
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int [] last=new int[26];
+        for(int i=0;i<s.length();i++){
+       last[s.charAt(i)-'a']=i;
+        }
+        List<Integer>ans=new ArrayList<>();
+        int start=0;
+        int end=0;
+        for(int i=0;i<s.length();i++){
+            end=Math.max(end,last[s.charAt(i)-'a']);
+            if(i==end){
+                ans.add(end-start+1);
+            
+            start=i+1;
+        }
+    }
+    return ans;
+}
+}
