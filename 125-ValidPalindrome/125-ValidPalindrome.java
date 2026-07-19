9948fdcb3596784321c8bbc5c57890317571b7ce
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-19 23:48:49 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,23 @@
+// Last updated: 7/19/2026, 11:47:55 PM
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray()){
+            
+            if(Character.isLetterOrDigit(c)){
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        int i=0;
+        int j=sb.length()-1;
+        while(i<j){
+            if(sb.charAt(i)!=sb.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+}
