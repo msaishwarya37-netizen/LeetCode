4e37327e03f7fb33950833ba0b6d6be75247735e
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-19 23:47:59 +0530

    Create 486-PredictTheWinner

---
+++ 486-PredictTheWinner/486-PredictTheWinner.java
@@ -0,0 +1,17 @@
+// Last updated: 7/19/2026, 11:47:04 PM
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        int[][]dp=new int[n][n];
+        for(int i=0;i<nums.length;i++)
+        dp[i][i]=nums[i];
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<=nums.length-len;i++){
+                int j=i+len-1;
+                dp[i][j]=Math.max(nums[i]-dp[i+1][j],nums[j]-dp[i][j-1]);
+            }
+        }
+return dp[0][n-1]>=0;
+        
+    }
+}
