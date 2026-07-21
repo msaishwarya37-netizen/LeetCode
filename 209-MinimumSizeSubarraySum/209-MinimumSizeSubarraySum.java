a1ff9fdd7d233f22239a9f0635fd2627a2a1977e
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-21 20:39:33 +0530

    File updated at 7/21/2026, 8:38:37 PM

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
@@ -1,20 +1,25 @@
-// Last updated: 7/21/2026, 8:01:36 PM
+// Last updated: 7/21/2026, 8:38:37 PM
 1class Solution {
-2    public int minSubArrayLen(int target, int[] nums) {
-3        int len=Integer.MAX_VALUE;
-4        
-5        int i=0;
-6            int sum=0;
-7            for(int j=0;j<nums.length;j++){
-8                sum+=nums[j];
-9                while(sum>=target){
-10                len=Math.min(len,j-i+1);
-11                sum-=nums[i];
-12                i++;
-13                }
-14            }
-15            
-16        
-17        return len==Integer.MAX_VALUE?0:len;
-18    }
-19}
+2    public int longestMountain(int[] arr) {
+3        int n=arr.length;
+4        int[] up=new int[n];
+5        int[] down=new int[n];
+6        for(int i=1;i<arr.length;i++){
+7        if(arr[i]>arr[i-1]){
+8        up[i]=up[i-1]+1;
+9    }
+10        }
+11    for(int i=n-2;i>=0;i--){
+12    if(arr[i]>arr[i+1]){
+13    down[i]=down[i+1]+1;
+14}
+15    }
+16int ans=0;
+17for(int i=0;i<arr.length;i++){
+18if(up[i]>0 && down[i]>0){
+19ans=Math.max(ans,up[i]+down[i]+1);
+20}
+21}
+22return ans;
+23}
+24}
