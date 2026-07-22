63c7a8e663930fd5ac48a7186c89684b6200ddcc
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-22 22:43:11 +0530

    File updated at 7/22/2026, 10:42:15 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -1,19 +1,19 @@
-// Last updated: 7/22/2026, 8:32:49 PM
+// Last updated: 7/22/2026, 10:42:15 PM
 1class Solution {
-2    public int numSubarrayProductLessThanK(int[] nums, int k) {
-3        if(k<=1)
-4        return 0;
-5        int left=0;
-6        int product=1;
-7        int count=0;
-8        for(int right=0;right<nums.length;right++){
-9         product*=nums[right];
-10         while(product>=k){
-11            product/=nums[left];
-12            left++;
-13         }
-14         count+=right-left+1;
-15        }
-16        return count;
+2    public List<Integer> findDuplicates(int[] nums) {
+3       HashMap<Integer,Integer> map=new HashMap<>();
+4       List<Integer> ans=new ArrayList<>();
+5       for(int i=0;i<nums.length;i++){
+6       if(map.containsKey(nums[i])){
+7         map.put(nums[i],map.get(nums[i])+1);
+8         if(map.get(nums[i])==2){
+9         ans.add(nums[i]);
+10         }
+11       }
+12         else{
+13         map.put(nums[i],1);
+14       }
+15       }
+16       return ans;
 17    }
 18}
