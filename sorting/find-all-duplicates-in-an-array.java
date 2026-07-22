9426c53af3ee203442196a06cc2efa244e691cb9
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-22 22:43:15 +0530

    LC-find-all-duplicates-in-an-array [Runtime: 29 ms; Memory: 70.2 MB]

---
+++ sorting/find-all-duplicates-in-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       List<Integer> ans=new ArrayList<>();
+       for(int i=0;i<nums.length;i++){
+       if(map.containsKey(nums[i])){
+         map.put(nums[i],map.get(nums[i])+1);
+         if(map.get(nums[i])==2){
+         ans.add(nums[i]);
+         }
+       }
+         else{
+         map.put(nums[i],1);
+       }
+       }
+       return ans;
+    }
+}
