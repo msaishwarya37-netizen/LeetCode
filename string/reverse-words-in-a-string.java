05ced6722b0d7c989f2af0e0e2cef3b6a402a94b
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-28 22:14:09 +0530

    LC-reverse-words-in-a-string [Runtime: 11 ms; Memory: 46.8 MB]

---
+++ string/reverse-words-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reverseWords(String s) {
+      String[] words=s.trim().split("\\s+");
+      String ans=words[words.length-1];
+      for(int i=words.length-2;i>=0;i--){
+        ans+=" "+words[i];
+      }  
+      return ans;
+    }
+}
