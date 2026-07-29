bad05114babb008cfdb79aca4a2461ee4d4952b5
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-29 18:20:04 +0530

    File updated at 7/29/2026, 6:19:07 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
@@ -1,18 +1,18 @@
-// Last updated: 7/29/2026, 6:05:20 PM
+// Last updated: 7/29/2026, 6:19:07 PM
 1class Solution {
 2    public int lengthOfLongestSubstring(String s) {
-3        int[]freq=new int[128];
+3        
 4      int left=0;
 5      int max=0;
-6      for(int right=0;right<s.length();right++){
-7        
-8        freq[s.charAt(right)]++;
-9        while(freq[s.charAt(right)]>1){
-10            freq[s.charAt(left)]--;
-11            left++;
-12        }
-13        max=Math.max(max,right-left+1);
-14      }  
+6      HashMap<Character,Integer>map=new HashMap<>();
+7      for(int right=0;right<s.length();right++){
+8        if(map.containsKey(s.charAt(right))){
+9            left=Math.max(left,map.get(s.charAt(right))+1);
+10        }
+11        map.put(s.charAt(right),right);
+12      
+13      max=Math.max(max,right-left+1);
+14      }
 15      return max;
 16    }
 17}
