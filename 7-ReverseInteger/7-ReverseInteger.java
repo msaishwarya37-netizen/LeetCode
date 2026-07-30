f1e8d555e930987cb5d7e1eda368868e8f88f745
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-30 23:04:42 +0530

    File updated at 7/30/2026, 11:03:46 PM

--- 7-ReverseInteger/7-ReverseInteger.java
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -1,17 +1,32 @@
-// Last updated: 7/30/2026, 10:12:31 PM
+// Last updated: 7/30/2026, 11:03:46 PM
 1class Solution {
-2    public int reverse(int x) {
-3        int rev=0;
-4        
-5        while(x!=0){
-6        int ldig=x%10;
-7        if(rev>Integer.MAX_VALUE/10||(rev==Integer.MAX_VALUE/10&&ldig>7))
-8        return 0;
-9        if(rev<Integer.MIN_VALUE/10||(rev==Integer.MIN_VALUE/10&&ldig<-8))
-10        return 0;
-11        rev=rev*10+ldig;
-12        x=x/10;
-13        }
-14        return rev;
-15            }
-16}
+2    public int myAtoi(String s) {
+3        int sign=1;
+4        int result=0;
+5        int i=0;
+6        while(i<s.length()&&s.charAt(i)==' '){
+7        i++;
+8        }
+9         if(i<s.length()&&(s.charAt(i)=='+'||s.charAt(i)=='-')){
+10            if(s.charAt(i)=='-'){
+11            sign=-1;
+12            }
+13            i++;
+14         }
+15         while(i<s.length()&&Character.isDigit(s.charAt(i))){
+16            int digit=s.charAt(i)-'0';
+17         
+18         if(result>Integer.MAX_VALUE/10 || (result==Integer.MAX_VALUE/10 &&digit>7)){
+19            if(sign==1)
+20            return Integer.MAX_VALUE;
+21            else
+22            return Integer.MIN_VALUE;
+23         }
+24         result=result*10+digit;
+25         i++;
+26    }
+27    return sign*result;
+28}
+29}
+30
+31
