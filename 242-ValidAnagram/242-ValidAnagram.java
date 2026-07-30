dd4f9576f1c49ec179312399251b4eb804e52d53
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-30 18:41:41 +0530

    File updated at 7/30/2026, 6:40:44 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,21 +1,24 @@
-// Last updated: 7/30/2026, 5:30:54 PM
-1class Solution {
-2    public boolean isAnagram(String s, String t) {
-3        if(s.length()!=t.length()){
-4            return false;
-5        }
-6     int[] count=new int[26];
-7
-8     for(int i=0;i<s.length();i++){
-9        count[s.charAt(i)-'a']++;
-10        count[t.charAt(i)-'a']--;
-11        
-12     }   
-13     for(int i=0;i<count.length;i++){
-14        if(count[i]!=0)
-15            return false;
-16        
-17     }
-18     return true;
-19    }
-20}
+// Last updated: 7/30/2026, 6:40:44 PM
+1import java.util.*;
+2
+3class Solution {
+4    public List<List<String>> groupAnagrams(String[] strs) {
+5
+6      HashMap<String,List<String>>map=new HashMap<>();
+7      for(int i=0;i<strs.length;i++){
+8        int[] count=new int[26];
+9        for(int j=0;j<strs[i].length();j++){
+10            count[strs[i].charAt(j)-'a']++;
+11        }
+12            String key="";
+13            for( int k=0;k<count.length;k++){
+14                key+="#"+count[k];
+15            }
+16            if(!map.containsKey(key)){
+17                map.put(key,new ArrayList<>());
+18            }
+19            map.get(key).add(strs[i]);
+20        }
+21        return new ArrayList<>(map.values());
+22      }
+23    }
