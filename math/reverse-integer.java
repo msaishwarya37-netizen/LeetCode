460dd394920299eb454f63662bf2a7ac0c3d3954
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-30 22:12:49 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.java
+++ math/reverse-integer.java
@@ -6,7 +6,7 @@ class Solution {
         int ldig=x%10;
         if(rev>Integer.MAX_VALUE||(rev==Integer.MAX_VALUE&&ldig>7))
         return 0;
-        if(rev<Integer.MIN_VALUE||(rev==Integer.MAX_VALUE&&ldig<-8))
+        if(rev<Integer.MIN_VALUE||(rev==Integer.MIN_VALUE&&ldig<-8))
         return 0;
         rev=rev*10+ldig;
         x=x/10;
