7e91e3371b136eac78fe7358008cfb21236be537
AishwaryaMS <msaishwarya37@gmail.com>
2026-07-31 14:01:06 +0530

    Create 674-LongestContinuousIncreasingSubsequence

---
+++ 674-LongestContinuousIncreasingSubsequence/674-LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,18 @@
+// Last updated: 7/31/2026, 2:00:08 PM
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+       int count=1;
+       int max=1;
+       for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+            count++;
+        }
+        else{
+            count=1;
+        }
+
+       max=Math.max(max,count);
+    }
+    return max;
+}
+}
