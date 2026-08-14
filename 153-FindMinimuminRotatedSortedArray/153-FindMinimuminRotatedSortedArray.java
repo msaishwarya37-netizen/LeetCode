2dcb558fb19d86016109d9623115c4c2f71c8f7b
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-14 23:36:16 +0530

    File updated at 8/14/2026, 11:35:15 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,18 +1,20 @@
-// Last updated: 8/14/2026, 11:21:11 PM
+// Last updated: 8/14/2026, 11:35:15 PM
 1class Solution {
-2    public char nextGreatestLetter(char[] letters, char target) {
-3       int low=0;
-4       int high=letters.length-1;
-5       while(low<=high){
-6        int mid=(low+high)/2;
-7        if(letters[mid]<=target){
-8            low=mid+1;
-9        }
-10        else{
-11            high=mid-1;
-12        }
-13       }
-14       return letters[low%letters.length];
-15
-16       }    
-17}
+2    public boolean isPerfectSquare(int num) {
+3        int low=1;
+4        int high=num;
+5        while(low<=high){
+6            int mid=(low+high)/2;
+7            if(mid==num/mid){
+8            if(num%mid==0){
+9                return true;
+10            }
+11        }
+12        if(mid<num/mid)
+13        low=mid+1;
+14        else
+15        high=mid-1;
+16    }
+17    return false;
+18    }
+19}
