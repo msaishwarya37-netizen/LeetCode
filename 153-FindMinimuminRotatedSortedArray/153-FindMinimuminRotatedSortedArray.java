4d9a2205e826a48d85ca21d351d1c6c2c3921378
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-14 23:04:06 +0530

    File updated at 8/14/2026, 11:03:05 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,26 +1,17 @@
-// Last updated: 8/14/2026, 10:49:59 PM
+// Last updated: 8/14/2026, 11:03:05 PM
 1class Solution {
-2    public int findMin(int[] nums) {
-3        int low = 0, high = nums.length - 1;
-4        while(low<high){
-5            int mid=(low+high)/2;
-6            if(nums[mid]>nums[high]){
-7                low=mid+1;
-8            }
-9             else{
-10             high=mid;   
-11        }
-12        }
-13        return nums[low];
+2    public int peakIndexInMountainArray(int[] arr) {
+3       int low=0;int high=arr.length-1;
+4       while(low<high){
+5        int mid=(low+high)/2;
+6        if(arr[mid]<arr[mid+1]){
+7            low=mid+1;
+8        }
+9        else{
+10        high=mid;
+11       }
+12       }
+13       return low;
 14    }
 15}
 16
-17
-18
-19
-20
-21
-22
-23
-24
-25        
