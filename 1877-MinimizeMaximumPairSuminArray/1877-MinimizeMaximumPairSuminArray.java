f7514be188c6c654981153b02aca6abce01e94b1
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-15 22:17:15 +0530

    File updated at 8/15/2026, 10:16:14 PM

--- 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -1,27 +1,18 @@
-// Last updated: 8/15/2026, 8:31:26 PM
+// Last updated: 8/15/2026, 10:16:14 PM
 1class Solution {
-2    public int[] searchRange(int[] nums, int target) {
-3        int first=find(nums,target,1);
-4        int last=find(nums,target,2);
-5        return new int[]{first,last};
-6    }
-7        int find(int[] nums,int target,int type){
-8        int low=0;int high=nums.length-1;
-9        int ans=-1;
-10        while(low<=high){
-11            int mid=(low+high)/2;
-12            if(nums[mid]==target){
-13                ans=mid;
-14            if(type==1)
-15                high=mid-1;
-16            else
-17                low=mid+1;
-18            }
-19            else if(nums[mid]<target)
-20                low=mid+1;
-21            else
-22            high=mid-1;
-23        }
-24        return ans;
-25    }
-26}
+2    public int countNegatives(int[][] grid) {
+3        int row=0;
+4        int col=grid[0].length-1;
+5        int count=0;
+6         while(row<grid.length && col>=0){
+7         if(grid[row][col]<0){
+8count+=grid.length-row;
+9col--;
+10         }
+11else{
+12row++;
+13         }
+14         }
+15         return count;
+16    }
+17}
