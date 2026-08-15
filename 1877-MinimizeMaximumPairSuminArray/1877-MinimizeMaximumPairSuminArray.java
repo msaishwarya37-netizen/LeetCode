237acff996d68c9f2723b05c961d7c50bb08f628
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-15 20:32:27 +0530

    File updated at 8/15/2026, 8:31:26 PM

--- 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -1,16 +1,27 @@
-// Last updated: 8/15/2026, 7:40:06 PM
+// Last updated: 8/15/2026, 8:31:26 PM
 1class Solution {
-2    public int minPairSum(int[] nums) {
-3        Arrays.sort(nums);
-4        int low=0;
-5        int high=nums.length-1;
-6        int maxSum=0;
-7        while(low<high){
-8            int sum=nums[low]+nums[high];
-9            maxSum=Math.max(maxSum,sum);
-10            low++;
-11            high--;
-12        }
-13        return maxSum;
-14    }
-15}
+2    public int[] searchRange(int[] nums, int target) {
+3        int first=find(nums,target,1);
+4        int last=find(nums,target,2);
+5        return new int[]{first,last};
+6    }
+7        int find(int[] nums,int target,int type){
+8        int low=0;int high=nums.length-1;
+9        int ans=-1;
+10        while(low<=high){
+11            int mid=(low+high)/2;
+12            if(nums[mid]==target){
+13                ans=mid;
+14            if(type==1)
+15                high=mid-1;
+16            else
+17                low=mid+1;
+18            }
+19            else if(nums[mid]<target)
+20                low=mid+1;
+21            else
+22            high=mid-1;
+23        }
+24        return ans;
+25    }
+26}
