b7aa0e3f7352110359b060b4289017969b853776
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-15 19:41:12 +0530

    LC-minimize-maximum-pair-sum-in-array [Runtime: 57 ms; Memory: 96.9 MB]

---
+++ greedy/minimize-maximum-pair-sum-in-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int low=0;
+        int high=nums.length-1;
+        int maxSum=0;
+        while(low<high){
+            int sum=nums[low]+nums[high];
+            maxSum=Math.max(maxSum,sum);
+            low++;
+            high--;
+        }
+        return maxSum;
+    }
+}
