08567d7fe8778a4af7e99acfde427ec4d8bc7d43
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-15 22:39:48 +0530

    File updated at 8/15/2026, 10:38:46 PM

--- 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -1,18 +1,16 @@
-// Last updated: 8/15/2026, 10:16:14 PM
+// Last updated: 8/15/2026, 10:38:46 PM
 1class Solution {
-2    public int countNegatives(int[][] grid) {
+2    public boolean searchMatrix(int[][] matrix, int target) {
 3        int row=0;
-4        int col=grid[0].length-1;
-5        int count=0;
-6         while(row<grid.length && col>=0){
-7         if(grid[row][col]<0){
-8count+=grid.length-row;
-9col--;
-10         }
-11else{
-12row++;
-13         }
-14         }
-15         return count;
-16    }
-17}
+4        int col=matrix[0].length-1;
+5        while(row<matrix.length && col>=0){
+6            if(matrix[row][col]==target)
+7            return true;
+8            if(matrix[row][col]>target)
+9            col--;
+10            else
+11            row++;
+12        }
+13        return false;
+14    }
+15}
