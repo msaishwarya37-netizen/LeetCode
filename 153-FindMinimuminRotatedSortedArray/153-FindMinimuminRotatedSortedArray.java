5e55023c3ad6d447631f1952959f4677ef357381
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-16 17:04:43 +0530

    File updated at 8/16/2026, 5:03:42 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,39 +1,22 @@
-// Last updated: 8/16/2026, 1:05:41 PM
-1
-2class Solution {
-3    public int findInMountainArray(int target, MountainArray mountainArr) {
-4        int low=0;
-5        int high=mountainArr.length()-1;
-6        while(low<high){
-7            int mid=(low+high)/2;
-8            if(mountainArr.get(mid)<mountainArr.get(mid+1))
-9                low=mid+1;
-10            else 
-11            high=mid;
-12    }
-13    int peak=low;
-14    low=0;
-15    high=peak;
-16    while(low<=high){
-17         int mid=(low+high)/2;
-18        if(mountainArr.get(mid)==target)
-19        return mid;
-20        if(mountainArr.get(mid)<target)
-21        low=mid+1;
-22        else
-23        high=mid-1;
-24    }
-25    low=peak+1;
-26    high=mountainArr.length()-1;
-27     while(low<=high){
-28         int mid=(low+high)/2;
-29        if(mountainArr.get(mid)==target)
-30        return mid;
-31        if(mountainArr.get(mid)>target)
-32        low=mid+1;
-33        else
-34        high=mid-1;
-35    }
-36    return -1; 
-37}
-38}
+// Last updated: 8/16/2026, 5:03:42 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode middleNode(ListNode head) {
+13 ListNode slow=head;
+14 ListNode fast=head;
+15 while(fast!=null && fast.next!=null){
+16slow=slow.next;
+17fast=fast.next.next;
+18 }
+19 return slow;
+20    }
+21}
