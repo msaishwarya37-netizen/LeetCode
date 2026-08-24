af96c81f2ca6fb9e91afc18decef252ddade3e9d
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-24 20:16:35 +0530

    File updated at 8/24/2026, 8:16:34 PM

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.java
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.java
@@ -1,4 +1,4 @@
-// Last updated: 8/24/2026, 8:03:16 PM
+// Last updated: 8/24/2026, 8:16:34 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,25 +10,13 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode partition(ListNode head, int x) {
-13        ListNode small= new ListNode(0);
-14        ListNode large=new ListNode(0);
-15ListNode s=small,l=large;
-16        while(head!=null){
-17            if(head.val<x){
-18                s.next=head;
-19                s=s.next;
-20            }
-21            else{
-22            l.next=head;
-23            l=l.next;
-24        }
-25        head=head.next;
-26        }
-27        l.next=null;
-28        s.next=large.next;
-29return small.next;
-30    }
-31    
-32
-33}
+12    public ListNode swapPairs(ListNode head) {
+13        if(head==null||head.next==null)
+14        return head;
+15        ListNode temp=head.next;
+16        head.next=swapPairs(temp.next);
+17        temp.next=head;
+18        return temp;
+19    }
+20
+21}
