5ef62e7ece5f91b8d410cc5f3e871c4e880bbe3a
AishwaryaMS <msaishwarya37@gmail.com>
2026-08-26 20:03:15 +0530

    File updated at 8/26/2026, 8:03:14 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.java
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 8/26/2026, 5:52:44 PM
+// Last updated: 8/26/2026, 8:03:14 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,16 +10,30 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode reverseList(ListNode head) {
-13     ListNode prev=null;
-14     ListNode curr=head;
-15     ListNode next=null;
-16     while(curr!=null){
-17        next=curr.next;
-18        curr.next=prev;
-19        prev=curr;
-20        curr=next;
-21     }
-22     return prev;
-23    }
-24}
+12    public void reorderList(ListNode head) {
+13        ListNode slow=head,fast=head;
+14        while(fast.next!=null && fast.next.next!=null){
+15            slow=slow.next;
+16            fast=fast.next.next;
+17        }
+18
+19        ListNode prev=null,curr=slow.next;
+20        slow.next=null;
+21
+22        while(curr!=null){
+23            ListNode next=curr.next;
+24            curr.next=prev;
+25            prev=curr;
+26            curr=next;
+27        }
+28        ListNode first=head;
+29        ListNode second=prev;
+30        while(second!=null){
+31            ListNode next=first.next;
+32            first.next=second;
+33            second=second.next;
+34            first.next.next=next;
+35            first=next;
+36        }
+37    }
+38}
