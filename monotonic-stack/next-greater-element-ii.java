49f871446a50ed3cee2f30b10c35750793d89119
AishwaryaMS <msaishwarya37@gmail.com>
2026-09-09 19:46:38 +0530

    LC-next-greater-element-ii [Runtime: 90 ms; Memory: 47.7 MB]

---
+++ monotonic-stack/next-greater-element-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+int n=nums.length;
+int [] ans=new int[n];
+for(int i=0;i<n;i++){
+    ans[i]=-1;
+    for(int j=1;j<n;j++){
+        int next=nums[(i+j)%n];
+        if(next>nums[i]){
+        ans[i]=next;
+        break;
+    }
+}
+    }
+    return ans;
+    }    
+}
