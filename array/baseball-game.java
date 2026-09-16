cb47f7ac91cda5d0969912d1b6b2d983ba2207a0
AishwaryaMS <msaishwarya37@gmail.com>
2026-09-16 23:11:52 +0530

    LC-baseball-game [Runtime: N/A; Memory: N/A]

--- array/baseball-game.java
+++ array/baseball-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public int calPoints(String[] operations) {
         Stack<Integer> st=new Stack<>();
-        for(int i=0;i<ops.length;i++){
-            String op=ops[i];
+        for(int i=0;i<operations.length;i++){
+            String op=operations[i];
             if(op.equals("C")){
                 st.pop();
             }
@@ -11,7 +11,7 @@ class Solution {
             }
             else if(op.equals("+")){
                 int last=st.pop();
-                int seclast=sr.peek();
+                int seclast=st.peek();
                 int sum=last+seclast;
                 st.push(last);
                 st.push(sum);
@@ -23,7 +23,7 @@ class Solution {
             }
         }
                 int total=0;
-                for(int i=0;i<st.size;i++){
+                for(int i=0;i<st.size();i++){
                     total+=st.get(i);
                 }
                 return total;
