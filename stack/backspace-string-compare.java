8a716710b3480504d9479902dad9e2b95f7e8f33
AishwaryaMS <msaishwarya37@gmail.com>
2026-09-17 19:52:16 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

---
+++ stack/backspace-string-compare.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> st1=new Stack<>();
+        Stack<Character> st2=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+             if(ch=='#'){
+            if(!s.isEmpty()){
+               st1.pop();
+            }
+             }
+            else{
+                st1.push(ch);
+            }
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(ch=='#'){
+                if(!t.isEmpty()){
+                    st2.pop();
+                }
+            }
+                else
+                {
+                    st2.push(ch);
+                }
+            }
+        
+            return st1.equals(st2);
+    }
+}
