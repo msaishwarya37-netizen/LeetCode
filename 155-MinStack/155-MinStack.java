11bb14c8fe4d255d4bc95bb673a9d95ce90979f2
AishwaryaMS <msaishwarya37@gmail.com>
2026-09-19 20:01:39 +0530

    File updated at 9/19/2026, 8:01:36 PM

--- 155-MinStack/155-MinStack.java
+++ 155-MinStack/155-MinStack.java
@@ -1,41 +1,40 @@
-// Last updated: 9/19/2026, 8:01:18 PM
+// Last updated: 9/19/2026, 8:01:36 PM
 1class MinStack {
 2 Stack<Integer> stack=new Stack<>();
 3       Stack<Integer> minStack=new Stack<>();
 4    public MinStack() {
-5       Stack<Integer> stack=new Stack<>();
-6       Stack<Integer> minStack=new Stack<>();
-7    }
-8    
-9    public void push(int value) {
-10         stack.push(value);
-11        if(minStack.isEmpty()||value<=minStack.peek()){
-12            minStack.push(value);
-13        }
-14       
-15    }
-16    
-17    public void pop() {
-18        if(minStack.peek().equals(stack.peek())){
-19            minStack.pop();
-20        }
-21        stack.pop();
-22    }
-23    
-24    public int top() {
-25      return stack.peek();
-26    }
-27    
-28    public int getMin() {
-29        return minStack.peek();
-30    }
-31}
-32
-33/**
-34 * Your MinStack object will be instantiated and called as such:
-35 * MinStack obj = new MinStack();
-36 * obj.push(value);
-37 * obj.pop();
-38 * int param_3 = obj.top();
-39 * int param_4 = obj.getMin();
-40 */
+5      
+6    }
+7    
+8    public void push(int value) {
+9         stack.push(value);
+10        if(minStack.isEmpty()||value<=minStack.peek()){
+11            minStack.push(value);
+12        }
+13       
+14    }
+15    
+16    public void pop() {
+17        if(minStack.peek().equals(stack.peek())){
+18            minStack.pop();
+19        }
+20        stack.pop();
+21    }
+22    
+23    public int top() {
+24      return stack.peek();
+25    }
+26    
+27    public int getMin() {
+28        return minStack.peek();
+29    }
+30}
+31
+32/**
+33 * Your MinStack object will be instantiated and called as such:
+34 * MinStack obj = new MinStack();
+35 * obj.push(value);
+36 * obj.pop();
+37 * int param_3 = obj.top();
+38 * int param_4 = obj.getMin();
+39 */
