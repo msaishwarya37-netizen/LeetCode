470621d9abc24b8fff31e56f9d40c60b2fa71de8
AishwaryaMS <msaishwarya37@gmail.com>
2026-09-21 23:09:23 +0530

    File updated at 9/21/2026, 11:09:20 PM

--- 227-BasicCalculatorII/227-BasicCalculatorII.java
+++ 227-BasicCalculatorII/227-BasicCalculatorII.java
@@ -1,35 +1,20 @@
-// Last updated: 9/21/2026, 8:20:57 PM
+// Last updated: 9/21/2026, 11:09:20 PM
 1class Solution {
-2    public String decodeString(String s) {
-3        Stack<Integer>count=new Stack<>();
-4        Stack<String>stack=new Stack<>();
-5        String result="";
-6        int num=0;
-7        for(int i=0;i<s.length();i++){
-8            char ch=s.charAt(i);
-9            if(Character.isDigit(ch)){
-10                num=num*10+(ch-'0');
-11            }
-12            else if(ch=='['){
-13                count.push(num);
-14                stack.push(result);
-15                result="";
-16                num=0;
-17            }
-18            else if(ch==']'){
-19                int n=count.pop();
-20                String old=stack.pop();
-21                String temp="";
-22                for(int j=0;j<n;j++){
-23                    temp+=result;
-24                }
-25                result=old+temp;
-26            }
-27            else
-28            {
-29                result+=ch;
-30            }
-31        }
-32        return result;
-33    }
-34}
+2    public int[] maxSlidingWindow(int[] nums, int k) {
+3      Deque<Integer>dq=new ArrayDeque<>();
+4      int j=0;
+5      int[]ans=new int[nums.length-k+1];
+6      for(int i=0;i<nums.length;i++){
+7        if(!dq.isEmpty()&& dq.peekFirst()<=i-k)
+8            dq.pollFirst();
+9        while(!dq.isEmpty()&& nums[dq.peekLast()]<=nums[i])
+10        dq.pollLast();
+11        dq.addLast(i);
+12        if(i>=k-1)
+13        ans[j++]=nums[dq.peekFirst()];
+14
+15      }
+16      return ans;
+17      }  
+18    }
+19
