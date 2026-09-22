88475a2e052c635eb5c1e21bc362fceb909a232f
AishwaryaMS <msaishwarya37@gmail.com>
2026-09-22 20:11:48 +0530

    File updated at 9/22/2026, 8:11:44 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,23 +1,20 @@
-// Last updated: 9/22/2026, 7:26:17 PM
+// Last updated: 9/22/2026, 8:11:44 PM
 1import java.util.*;
 2
 3class Solution {
-4    public int largestRectangleArea(int[] h) {
+4    public int largestRectangleArea(int[] heights) {
 5        Stack<Integer> s = new Stack<>();
 6        int max = 0;
 7
-8        for (int i = 0; i <= h.length; i++) {
-9            int curr = (i == h.length) ? 0 : h[i];
-10
-11            while (!s.isEmpty() && h[s.peek()] > curr) {
-12                int height = h[s.pop()];
-13                int width = s.isEmpty() ? i : i - s.peek() - 1;
-14                max = Math.max(max, height * width);
-15            }
-16
-17            s.push(i);
-18        }
-19
-20        return max;
-21    }
-22}
+8        for(int i=0;i<=heights.length;i++){
+9            int curr=(i==heights.length)?0:heights[i];
+10            while(!s.isEmpty()&&heights[s.peek()]>curr){
+11                int height=heights[s.pop()];
+12                int width=s.isEmpty()?i:i-s.peek()-1;
+13                max=Math.max(max,height*width);
+14            }
+15            s.push(i);
+16        }
+17        return max;
+18    }
+19}
