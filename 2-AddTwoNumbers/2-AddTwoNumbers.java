f177f3f9d52e58877f3de4a2336b7a3a0dd9c1c1
AishwaryaMS <msaishwarya37@gmail.com>
2026-09-22 23:02:45 +0530

    File updated at 9/22/2026, 11:02:41 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,20 +1,37 @@
-// Last updated: 9/22/2026, 8:11:44 PM
-1import java.util.*;
-2
-3class Solution {
-4    public int largestRectangleArea(int[] heights) {
-5        Stack<Integer> s = new Stack<>();
-6        int max = 0;
-7
-8        for(int i=0;i<=heights.length;i++){
-9            int curr=(i==heights.length)?0:heights[i];
-10            while(!s.isEmpty()&&heights[s.peek()]>curr){
-11                int height=heights[s.pop()];
-12                int width=s.isEmpty()?i:i-s.peek()-1;
-13                max=Math.max(max,height*width);
-14            }
-15            s.push(i);
-16        }
-17        return max;
-18    }
-19}
+// Last updated: 9/22/2026, 11:02:41 PM
+1class LRUCache {
+2    int capacity;
+3HashMap<Integer,Integer> map=new HashMap<>();
+4LinkedList<Integer>list=new LinkedList<>();
+5    public LRUCache(int capacity) {
+6        this.capacity=capacity;
+7    }
+8    
+9    public int get(int key) {
+10    if(!map.containsKey(key))
+11      return -1;
+12      list.remove((Integer)key);
+13      list.addLast(key);
+14      return map.get(key);  
+15    }
+16    
+17    public void put(int key, int value) {
+18     if(map.containsKey(key)){
+19        list.remove((Integer)key);
+20     }
+21     map.put(key,value);
+22     list.addLast(key);
+23        if(list.size()>capacity){
+24            int old=list.removeFirst();
+25            map.remove(old);
+26        }
+27     
+28    }
+29}
+30
+31/**
+32 * Your LRUCache object will be instantiated and called as such:
+33 * LRUCache obj = new LRUCache(capacity);
+34 * int param_1 = obj.get(key);
+35 * obj.put(key,value);
+36 */
